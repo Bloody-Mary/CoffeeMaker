6e7a05a7d850e98b7cb8fa4a22829ee7cb78ccf9
mary <marianelalieben@gmail.com>
2024-03-01 16:31:58 +0300

    init:01.03.2024 16:31 project and enum class were created.

---
+++ src/main/java/org/coffeeMaker/CoffeeCondition.java
@@ -0,0 +1,19 @@
+package org.coffeeMaker;
+
+public enum CoffeeCondition {
+    ACTION,
+    BUY,
+    FILL,
+    TAKE,
+    RAMAINING,
+    EXIT,
+    ADD_WATER,
+    ADD_MILK,
+    ADD_BEANS,
+    ADD_CUP,
+    ADD_SUGAR,
+    CAPPUCCINO,
+    ESPRESSO,
+    AMERICANO,
+    LATTE
+}
