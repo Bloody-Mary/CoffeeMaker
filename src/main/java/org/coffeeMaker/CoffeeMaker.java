4547a2432e96f18be65e832d53003b945b5ab420
mary <marianelalieben@gmail.com>
2024-03-01 16:32:56 +0300

    feature:01.03.2024 16:32 fields were created.

---
+++ src/main/java/org/coffeeMaker/CoffeeMaker.java
@@ -0,0 +1,10 @@
+package org.coffeeMaker;
+
+public class CoffeeMaker {
+    static int currentPrice = 0;
+    static int cupsAmount = 0;
+    static int waterAmount = 0;
+    static int milkAmount = 0;
+    static int beansAmount = 0;
+    static CoffeeCondition coffeeCondition;
+}
